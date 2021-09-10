b149e78196dc4306dfd5e2544392c18d99cf6469
AmirHossein Esmailie <69587936+amirhosseine13579@users.noreply.github.com>
2021-09-10 19:39:09 +0430

    Add files via upload

---
+++ precache-manifest.76341b27c718e6716f578cd8c33f451c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b80e3e82252d36a87f5dfe52709e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a0e2514456102e2f1e",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "f09c52ee8a9c2c1a0876",
+    "url": "/static/css/main.61db3b33.chunk.css"
+  },
+  {
+    "revision": "79a0e2514456102e2f1e",
+    "url": "/static/js/2.4088df85.chunk.js"
+  },
+  {
+    "revision": "074a984e3d6f2df40aaacb159df0c8bf",
+    "url": "/static/js/2.4088df85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09c52ee8a9c2c1a0876",
+    "url": "/static/js/main.b5a8febc.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
